5dfe9fccfdf486ebf7d9ed2bcc71b6fed6480bde
kairavkkp <kairavpithadia13@gmail.com>
2020-12-28 10:30:15 +0530

    Updates

---
+++ precache-manifest.6788c25b052d08d90c20203923b192fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b62909511ee718c14f16617550cfb3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "303f8dc6114924c1f2ea",
+    "url": "/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "e2d7782296920a426140",
+    "url": "/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "303f8dc6114924c1f2ea",
+    "url": "/static/js/2.d5b39d8c.chunk.js"
+  },
+  {
+    "revision": "86bc53d52472cf4ce0483f03cfa8a523",
+    "url": "/static/js/2.d5b39d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d7782296920a426140",
+    "url": "/static/js/main.9c5dc5f8.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "53c7a9eda87f7180c4761e5d3ee7db05",
+    "url": "/static/media/resume.53c7a9ed.pdf"
+  }
+]);
